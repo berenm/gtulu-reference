d4f578e666969f743270def997af4b2ae97c1182
Beren Minor <beren.minor+git@gmail.com>
2012-12-25 16:34:47 +0100

    Fix unit test that was using an API call from GL4.3 whereas the default version is 3.3.

--- test/test1.cpp
+++ test/test1.cpp
@@ -4,7 +4,5 @@ int main(int argc, char const* argv[]) {
   gtulu::api::active_texture(0);
   gtulu::api::attach_shader(1, 2);
 
-  gtulu::api::program_uniform_matrix_4x3(0, 1, 2, true, (double*) nullptr);
-
   return 0;
 }
