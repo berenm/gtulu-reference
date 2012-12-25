d14ae7a1453c15c634e0c5e83c506ca2bfa05d62
Beren Minor <beren.minor+git@gmail.com>
2012-12-25 16:27:09 +0100

    Update CMakeLists.txt to generate only one version of the library. Configurable via libgtulu_api_VERSION CMake variable.

--- test/test1.cpp
+++ test/test1.cpp
@@ -4,5 +4,7 @@ int main(int argc, char const* argv[]) {
   gtulu::api::active_texture(0);
   gtulu::api::attach_shader(1, 2);
 
+  gtulu::api::program_uniform_matrix_4x3(0, 1, 2, true, (double*) nullptr);
+
   return 0;
 }
